c0cf67617d2bd0e8bf8d1480d25c4ac5703802dd
SantanuDas2345 <90392244+SantanuDas2345@users.noreply.github.com>
2023-01-10 00:52:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [st, setSt] = useState(true);
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{st ? 'Marco' : 'Polo'}</h1>
+      <button id="marco-polo-toggler" onClick={() => setSt(st => !st)}>{st ? 'Polo' : 'Marco'}</button>
     </div>
   )
 }
